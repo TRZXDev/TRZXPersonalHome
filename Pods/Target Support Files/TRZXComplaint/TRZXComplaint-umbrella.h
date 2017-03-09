d3466adec2c079c5df6f82683db256b5a54f5acb
zhangjw@mmwipo.com <zhangjw@mmwipo.com>
2017-03-09 13:51:04 +0800

    编辑.podspec

---
+++ Pods/Target Support Files/TRZXComplaint/TRZXComplaint-umbrella.h
@@ -0,0 +1,23 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ComplaintsTitleTableViewCell.h"
+#import "CTMediator+TRZXComplaint.h"
+#import "Target_TRZXComplaint.h"
+#import "TRZXComplaintCell.h"
+#import "TRZXComplaintsMsgViewController.h"
+#import "TRZXComplaintsViewController.h"
+#import "TRZXComplaintViewModel.h"
+
+FOUNDATION_EXPORT double TRZXComplaintVersionNumber;
+FOUNDATION_EXPORT const unsigned char TRZXComplaintVersionString[];
+
